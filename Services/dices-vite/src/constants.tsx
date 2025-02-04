ad2f809d4b38a9b6d8277653b3c35f2e3570e6f5
antty <antty@antty.antty>
2025-02-04 08:39:52 -0600

    Constant IP Changed to localhost

--- Services/dices-vite/src/constants.tsx
+++ Services/dices-vite/src/constants.tsx
@@ -1,2 +1,2 @@
-//export const dices_URL = 'http://127.0.0.1:82';
-export const dices_URL = 'http://192.168.1.38:82';
+export const dices_URL = 'http://127.0.0.1:82';
+//export const dices_URL = 'http://192.168.1.38:82';
