1041091bb300a09823bad19422a5a2dc7352a48c
Mario Gómez Garcia <134717135+OneProgrammerMore@users.noreply.github.com>
2025-03-21 17:54:20 +0100

    Update constants.tsx

--- Services/dices-vite/src/constants.tsx
+++ Services/dices-vite/src/constants.tsx
@@ -1,3 +1,4 @@
 export const dices_URL = 'http://127.0.0.1:82';
 export const dices_React_URL = 'http://127.0.0.1:82';
-//export const dices_URL = 'http://192.168.1.38:82';
+export const dices_Router_Prefix = ""
+
